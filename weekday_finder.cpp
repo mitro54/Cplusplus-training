6e28ff0662f619982057bddcc3bff4e004ece3bf
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-10-09 22:24:25 +0300

    Add zeller's congruence

--- weekday_finder.cpp
+++ weekday_finder.cpp
@@ -3,7 +3,7 @@
 int main(void) {
     int dd, mm, yy;
 
-    std::cout << "Give date in three parts to find out what weekday was on that date.";
+    std::cout << "Give date in three parts to find out what weekday was on that date.\n";
 
     std::cout << "Day: ";
     std::cin >> dd;
@@ -14,5 +14,21 @@ int main(void) {
     std::cout << "Year: ";
     std::cin >> yy;
 
+    // Zeller's congruence
+    mm -= 2;
+    if (mm <= 0) {
+        mm += 12;
+        yy -= 1;
+    }
+    mm = (mm *= 83) / 32;
+    mm += dd;
+    mm += yy;
+    mm += (yy / 4);
+    mm -= (yy / 100);
+    mm += (yy / 400);
+    mm %= 7;
+
+    std::cout << "0: Sunday, 1: Monday, 2: Tuesday, 3: Wednesday, 4: Thursday, 5: Friday, 6: Saturday\n";
+    std::cout << "Weekday on the given date: " << mm;
     return 0;
 }
