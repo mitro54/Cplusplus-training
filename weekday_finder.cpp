5920fa8348ba488be711ae7045817bec0391395e
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-10-09 19:21:33 +0300

    Add basic I/O structure

--- weekday_finder.cpp
+++ weekday_finder.cpp
@@ -2,5 +2,17 @@
 
 int main(void) {
     int dd, mm, yy;
+
+    std::cout << "Give date in three parts to find out what weekday was on that date.";
+
+    std::cout << "Day: ";
+    std::cin >> dd;
+
+    std::cout << "Month: ";
+    std::cin >> mm;
+
+    std::cout << "Year: ";
+    std::cin >> yy;
+
     return 0;
 }
