c402498de41a8284028f74ecd62218e0c5359ab5
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-10-09 15:42:11 +0300

    Add conversion to metric from imperial

--- measurement_converter.cpp
+++ measurement_converter.cpp
@@ -7,12 +7,23 @@ int main(void) {
     std::cout << "Which system are you converting to?\n1 for metric, 2 for imperial: ";
     std::cin >> msys;
     if (msys == 1) {
+
         // metric
+        std::cout << "Enter measurement in 2 parts.. first ft... : ";
+        std::cin >> ft;
+        std::cout << "... and inches: ";
+        std::cin >> in;
+        float total_in = (ft * 12) + in; 
+        std::cout << "Measurement in meters: " << total_in * .0254 << "m";
+
     } else if (msys == 2) {
         // imperial
+        std::cout << "Enter measurement in meters: ";
+        std::cin >> m;
     } else {
-        std::cout << "unrecognized system, select 1 or 2";
+        std::cout << "Unrecognized system, select 1 or 2";
     }
+
 
     return 0;
 }
