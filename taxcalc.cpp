0b7a1549f413459ecd398d92211d94447d56d6ef
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-10-09 14:44:21 +0300

    Add some safeguards to logic

--- taxcalc.cpp
+++ taxcalc.cpp
@@ -7,10 +7,14 @@ int main(void) {
     std::cout << "Enter a tax rate: ";
     std::cin >> tax;
 
-    net = gross / (1 + (tax / 100));
-    taxval = gross - net;
-    std::cout << "Net price: " << net << std::endl;
-    std::cout << "Tax value: " << taxval << std::endl;
+    if (gross > 0 && tax <= 100 && tax >= 0) {
+        net = gross / (1 + (tax / 100));
+        taxval = gross - net;   
+        std::cout << "Net price: " << net << std::endl;
+        std::cout << "Tax value: " << taxval << std::endl;   
+    } else {
+        std::cout << "Price must be over 0 and tax rate between 0-100";
+    }
 
     return 0;
 }
