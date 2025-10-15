d677c56bdbf7c23d47bb61fb38d13c182a6bcf0e
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-10-15 15:52:42 +0300

    update matrixpointer.cpp

--- matrixpointer.cpp
+++ matrixpointer.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 
 int main(void) {
+
 	int matrix[10][10] = { };
-	int (*p)[10] = matrix;
+
+    int *p = (int *)matrix;
+
+    for (int x = 0; x < 10; x++) {
+        for (int y = 0; y < 10; y++) {
+            
+            int *q = p + (x * 10 + y);
+            *q = (x + 1) * (y + 1);
+        }
+    }
 
 	for(int i = 0; i < 10; i++) {
 		for(int j = 0; j < 10; j++) {
-            matrix[i][j] = (i + 1) * (j + 1);
 			std::cout.width(4);
 			std::cout << matrix[i][j];
 		}
