6aea0e7003ea498e988fb562123f62b9da35e48a
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-10-17 20:10:23 +0300

    Modify bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,33 +1,32 @@
 #include <iostream>
 
 int main(void) {
-  int numbers[5];
-  int aux;
-  bool swap;
-  
-  // asks the user to enter 5 values 
-  for (int i = 0; i < 5; i++) {
-    std::cout << std::endl << "Enter value #" << i + 1 << ": ";
-    std::cin >> numbers[i];
-  }
 
-  // sort the array 
-  do {
-    swap = false;
-    for (int i = 0; i < 4; i++) {
-      if (numbers[i] > numbers[i + 1]) {
-        swap = true;
-        aux = numbers[i];
-        numbers[i] = numbers[i + 1];
-        numbers[i + 1] = aux;
-      }
+    int array[5];
+    int aux;
+    bool swap;
+
+    std::cout << "Enter 5 values to sort them out. \n";
+
+    for (int i = 0; i < 5; i++) {
+        std::cout << "Give value #" << i + 1 << ": ";
+        std::cin >> array[i];
     }
-  } while (swap);
 
-  // print the results
-  std::cout << std::endl << "Sorted: " << std::endl;
-  for (int i = 0; i < 5; i++)
-    std::cout << numbers[i] << " ";
-  std::cout << std::endl;
-  return 0;
+    do {
+        swap = false;
+        for (int i = 0; i < 4; i++) {
+            if (array[i] > array[i + 1]) {
+                swap = true;
+                aux = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = aux;
+            }
+        }
+    } while (swap);
+
+    for (int i = 0; i < 5; i++) {
+    std::cout << array[i] << " ";
+    }
+    return 0;
 }
