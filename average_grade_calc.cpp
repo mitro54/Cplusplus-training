c9c5097c40da4c37cd57e632529fc642562485f7
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-10-20 19:40:47 +0300

    Update average_grade_calc.app
    - works and cleans up for 1 iteration

--- average_grade_calc.cpp
+++ average_grade_calc.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
 
 int main(void) {
-    int courses, grade_count;
+    int courses, grade_count, **arr;
     std::cout << "Enter number of total courses: ";
     std::cin >> courses;
 
     for (int i = 0; i < courses; i++) {
         // usage: num of grades followed by num1 num2 num3 ... example: 3 2 2 1
-        std::cout << "Enter course #" << i + 1 << " grades: ";
+        std::cout << "\nEnter course #" << i + 1 << " grades: ";
         std::cin >> grade_count;
-        int *arr = new int[grade_count];
-        for (int i = 0; i < grade_count; i++) {
-            std::cin >> arr[i];
+        arr = new int *[courses];
+        for (int r = 0; r < courses; r++) {
+            arr[r] = new int[grade_count];
+            for (int c = 0; c < grade_count; c++)
+                std::cin >> arr[r][c];
         }
 
         std::cout << "entered: ";
-        for (int i = 0; i < grade_count; i++) {
-            std::cout << arr[i] << " ";
+        for (int i = 0; i < courses; i++) {
+            for (int j = 0; j < grade_count; j++) {
+                std::cout << arr[i][j] << " ";
+            }
         }
 
+        for (int i = 0; i < courses; i++)
+            delete[] arr[i];
         delete[] arr;
 
     }
