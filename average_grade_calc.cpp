3edc239ea75c032ffca5b76cb0941c63afac65f4
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-10-21 11:58:02 +0300

    Update average_grade_calc.cpp
    - Works now for loops

--- average_grade_calc.cpp
+++ average_grade_calc.cpp
@@ -4,29 +4,29 @@ int main(void) {
     int courses, grade_count, **arr;
     std::cout << "Enter number of total courses: ";
     std::cin >> courses;
+    arr = new int *[courses];
 
     for (int i = 0; i < courses; i++) {
         // usage: num of grades followed by num1 num2 num3 ... example: 3 2 2 1
         std::cout << "\nEnter course #" << i + 1 << " grades: ";
         std::cin >> grade_count;
-        arr = new int *[courses];
-        for (int r = 0; r < courses; r++) {
-            arr[r] = new int[grade_count];
-            for (int c = 0; c < grade_count; c++)
-                std::cin >> arr[r][c];
-        }
+        arr[i] = new int[grade_count];
+        int counts[grade_count];
+        
+        for (int k = 0; k < grade_count; k++)
+            counts[k] = grade_count;
+
+        for (int c = 0; c < counts[i]; c++)
+            std::cin >> arr[i][c];
 
         std::cout << "entered: ";
-        for (int i = 0; i < courses; i++) {
-            for (int j = 0; j < grade_count; j++) {
-                std::cout << arr[i][j] << " ";
-            }
+        for (int j = 0; j < counts[i]; j++) {
+            std::cout << arr[i][j] << " ";
         }
-
+    }
         for (int i = 0; i < courses; i++)
             delete[] arr[i];
         delete[] arr;
 
-    }
     return 0;
 }
