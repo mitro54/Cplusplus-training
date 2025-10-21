a7808a2a60c744482be586ca7ebfc46d11987520
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-10-21 19:00:40 +0300

    Finish average_grade_calc.cpp

--- average_grade_calc.cpp
+++ average_grade_calc.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <iomanip>
 
 int main(void) {
-    int courses, grade_count, **arr;
+    int courses, grade_count, **arr, loop_count;
+    float total = 0;
     std::cout << "Enter number of total courses: ";
     std::cin >> courses;
     arr = new int *[courses];
@@ -12,6 +14,8 @@ int main(void) {
         std::cin >> grade_count;
         arr[i] = new int[grade_count];
         int counts[grade_count];
+        float loop_total = 0;
+        loop_count = 0;
         
         for (int k = 0; k < grade_count; k++)
             counts[k] = grade_count;
@@ -19,11 +23,23 @@ int main(void) {
         for (int c = 0; c < counts[i]; c++)
             std::cin >> arr[i][c];
 
-        std::cout << "entered: ";
+        std::cout << "Course " << i + 1 << ": ";
+        for (int j = 0; j < counts[i]; j++) {
+            loop_total += arr[i][j];
+            loop_count += 1;
+        }
+        total += (loop_total / loop_count);
+        std::cout << "final " 
+        << std::fixed << std::setprecision(2)
+        << loop_total / loop_count
+        << ", grades: ";
+
         for (int j = 0; j < counts[i]; j++) {
             std::cout << arr[i][j] << " ";
         }
     }
+    std::cout << "\nOverall final " << total / courses;
+
         for (int i = 0; i < courses; i++)
             delete[] arr[i];
         delete[] arr;
