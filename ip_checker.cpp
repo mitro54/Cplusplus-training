cd5602e98e79b091449ca407a0825e0234cdfda2
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-10-24 17:36:36 +0300

    Add logic ip_checker.cpp

--- ip_checker.cpp
+++ ip_checker.cpp
@@ -3,19 +3,26 @@
 
 std::string ip_checker(std::string ip) {
     int count = 0;
+    std::string tester = "0123456789.";
     for (int i = 0; i < ip.length(); i++) {
-       if (ip.find('.') != std::string::npos) count++;
+        if (tester.find(ip[i]) == std::string::npos)
+            return "Only digits and dots allowed";
+
+        if (ip[i] == '.') count++;
     }
-    if (count < 4) {
-        return;
+
+    if (count != 3) {
+        std::cout << count;
+        return "Incorrect parts count";
     }
-    return;
+
+    return "ok";
 }
 
 int main(void) {
     std::string ip;
     std::cout << "insert IP: ";
     std::cin >> ip;
-    ip_checker(ip);
+    std::cout << ip_checker(ip);
     return 0;
 }
