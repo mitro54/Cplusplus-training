8321e39c6bd42b48047179aa9cb6db26761492d8
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-10-24 22:28:58 +0300

    Remove print, fixes

--- whitespace_dupl_checker.cpp
+++ whitespace_dupl_checker.cpp
@@ -7,14 +7,13 @@ std::string whitespace_cleaner(std::string str) {
     for (int i = 0; i < str.length(); i++) {
         if (str[i] == ' ' && str[i + 1] == ' ') {
             start = i;
-            for (int j = i; j < str.length(); j++) {
+            for (int j = i; j <= str.length(); j++) {
                 if (str[j] != ' ') {
                     end = j - 1;
                     i = j - 1;
                     break;
                 }
             }
-            std::cout << start << " " << end - start;
             str.erase(start, end - start);
         }
     }
@@ -23,8 +22,8 @@ std::string whitespace_cleaner(std::string str) {
 
 int main(void) {
     std::string str;
-    // std::cout << "Give a string: ";
-    // std::getline(std::cin, str);
-    std::cout << whitespace_cleaner("test  ing          test i  ng");
+    std::cout << "Give a string: ";
+    std::getline(std::cin, str);
+    std::cout << whitespace_cleaner(str);
     return 0;
 }
