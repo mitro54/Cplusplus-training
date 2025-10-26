edac948de8a1281c28e42c9f323fd04f4aafcb20
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-10-26 20:31:41 +0200

    Save, for later

---
+++ text_formatting.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+
+int main() {
+    std::string sentence;
+    std::getline(std::cin, sentence);
+
+    for (int i = 0; i < sentence.length(); i++) {
+        if (sentence[i] == '*') {
+            
+        }
+    }
+
+    std::cout << sentence << "\n";
+}
