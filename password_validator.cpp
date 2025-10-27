dfcc7e80742dc18f2ff63ac439d4dbe0ed0d48ba
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-10-27 16:05:08 +0200

    Update logic

--- password_validator.cpp
+++ password_validator.cpp
@@ -8,10 +8,19 @@ int main() {
     std::cout << "Enter a password: ";
     std::getline(std::cin, password);
 
+    int d = 0, a = 0, p = 0, l = 0;
     for (int i = 0; i < password.length(); i++) {
-        if (isdigit(password[i])) std::cout << "yes";
-        // isdigit(char), isalpha(char) is_punct(char)
+        if (isdigit(password[i])) d++;
+        if (isalpha(password[i])) a++;
+        if (ispunct(password[i])) p++;
+        if (password.length() > 8) l++;
+        // isdigit(char), isalpha(char) ispunct(char)
     }
+
+    if (d == 0) std::cout << "The password must have at least one digit\n";
+    if (a == 0) std::cout << "The password must have at least one upper case letter\n"; // doesnt work yet
+    if (p == 0) std::cout << "The password must have at least one digit\n";
+    if (l == 0) std::cout << "The password must be 8 characters long\n";
 
     return 0;
 }
