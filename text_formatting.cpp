f4b081a2a36522dc5edaa1ac6435c4cf6f334625
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-10-27 13:12:25 +0200

    Fix syntax, has bugs

--- text_formatting.cpp
+++ text_formatting.cpp
@@ -12,16 +12,17 @@ int main() {
     for (int i = 0; i < sentence.length(); i++) {
         if (sentence[i] == '*') {
             if (sentence.find('*', i + 1) == std::string::npos) continue;
-            else for (int j = i; j < sentence.length(); j++) {
+            else for (int j = i + 1; j < sentence.length(); j++) {
                 if (sentence[j] == '_') {
                     sentence.erase(i, 1);
                     i = j - 1;
                     break;
 
                 } else if (sentence[j] == '*') {
-                    std::string newstr = sentence.substr(sentence[i + 1], sentence[j] - sentence[i + 1]);
-                    sentence.erase(sentence[i], sentence[j] - sentence[i]);
-                    sentence.insert(sentence[i], newstr);
+                    std::string newstr = sentence.substr(i + 1, j - (i + 1));
+                    sentence.erase(i, j - i);
+                    sentence.insert(i, newstr);
+                    break;
                 }
             }
         }
