072b3408269416026c451d93da631fe9e5547496
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-10-28 14:27:48 +0200

    Add rest of the logic to script

--- text_templating.cpp
+++ text_templating.cpp
@@ -6,20 +6,22 @@ std::string key_formatter(std::string k) {
 }
 
 std::string val_processor(std::string vals, std::string txt_template) {
-    std::string curr_val, key, keys_val, newstr;
+    std::string curr_val, key, keys_val;
     for (int i = 0; i < vals.length(); i++) {
         if (vals.find(',') != std::string::npos) {
-            curr_val = vals.substr(i, vals.find(','));
+            curr_val = vals.substr(0, vals.find(','));
             key = curr_val.substr(0, vals.find('='));
             keys_val = curr_val.substr(vals.find('=') + 1);
-            vals.erase(i, vals.find(',') + 1);
+            vals.erase(0, curr_val.length() + 1);
+            i = 0;
         } else {
             curr_val = vals;
             key = curr_val.substr(0, vals.find('='));
             keys_val = curr_val.substr(vals.find('=') + 1);
             int start_pos = txt_template.find(key_formatter(key));
             txt_template.replace(start_pos, key_formatter(key).length(), keys_val);
-            break;
+            if (txt_template.find(key_formatter(key)) != std::string::npos) continue;
+            else break;
         }
         int start_pos = txt_template.find(key_formatter(key));
         txt_template.replace(start_pos, key_formatter(key).length(), keys_val);
@@ -28,15 +30,16 @@ std::string val_processor(std::string vals, std::string txt_template) {
 }
 
 int main(void) {
+    std::cout << "Text template translator. First set a template like person=John,action=meet\n";
+    std::cout << "then give a text, example: Hello [person], its nice to [action] you.\n";
+    std::cout << "It is translated to Hello John, its nice to meet you.\n";
+    std::cout << "Set a template: ";
     std::string values;
-    // std::getline(std::cin, values);
+    std::getline(std::cin, values);
 
+    std::cout << "Give a text: ";
     std::string txt_template;
-    // std::getline(std::cin, txt_template);
-    values = "place=world,type=beautiful";
-    txt_template = "say hello to [place] friend, isnt it [type] here?";
-
-    // Put values into the template
-    std::cout << val_processor(values, txt_template) << "\n"; 
+    std::getline(std::cin, txt_template);
+    std::cout << val_processor(values, txt_template) << "\n";
     return 0;
 }
