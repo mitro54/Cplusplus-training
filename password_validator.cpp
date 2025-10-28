0efff5b60645ad0fd027b320bcb5a2f960ef0b63
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-10-28 11:25:00 +0200

    finish script

--- password_validator.cpp
+++ password_validator.cpp
@@ -8,18 +8,28 @@ int main() {
     std::cout << "Enter a password: ";
     std::getline(std::cin, password);
 
-    int d = 0, a = 0, p = 0, l = 0;
+    int d = 0, lc_a = 0, uc_a = 0, p = 0, l = 0;
+    std::string ltr_case = "lower";
     for (int i = 0; i < password.length(); i++) {
-        if (isdigit(password[i])) d++;
-        if (isalpha(password[i])) a++;
-        if (ispunct(password[i])) p++;
         if (password.length() > 8) l++;
-        // isdigit(char), isalpha(char) ispunct(char)
+        if (isdigit(password[i])) d++;
+        if (ispunct(password[i])) p++;
+        if (isalpha(password[i])) {
+            if (toupper(password[i]) == password[i]) uc_a++;
+            if (tolower(password[i]) == password[i]) lc_a++;
+        }
     }
+    if (uc_a == 0) ltr_case = "upper";
     if (l == 0) std::cout << "The password must be 8 characters long\n";
     if (d == 0) std::cout << "The password must have at least one digit\n";
-    if (a == 0) std::cout << "The password must have at least one upper case letter\n"; // doesnt work yet
+    if (lc_a == 0 || uc_a == 0) std::cout << "The password must have at least one " << ltr_case << " case letter\n"; // doesnt work yet
     if (p == 0) std::cout << "The password must have at least one special character\n";
-
+    if (
+        l != 0
+        && d != 0
+        && p != 0
+        && lc_a != 0
+        && uc_a != 0
+        ) std::cout << "The password is valid";
     return 0;
 }
