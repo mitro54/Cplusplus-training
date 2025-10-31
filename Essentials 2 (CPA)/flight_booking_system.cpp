9369b276670b2ccc7fab56fc90717df307b6fc13
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-10-31 22:38:08 +0200

    update logic to work in a loop, supporting multiple bookings

--- Essentials 2 (CPA)/flight_booking_system.cpp
+++ Essentials 2 (CPA)/flight_booking_system.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <vector>
 
 class FlightBooking {
 public:
-  FlightBooking(int id, int capacity, int reserved);
+  FlightBooking(int id, int capacity, int reserved = 0);
   void printStatus();
 
   // getters
@@ -14,7 +15,6 @@ public:
   // setters
   void setCapacity(int capacity) { this->capacity = capacity; };
   void setReserved(int reserved) { this->reserved = reserved; };
-  void setId(int id) { this->id = id;}
 
 private:
   int id;
@@ -41,70 +41,69 @@ FlightBooking::FlightBooking(int id, int capacity, int reserved) {
 }
 
 int main() {
-  int reserved = 0, capacity = 0;
-  std::cout << "No flights in the system\n";
-  std::cout << "Provide flight capacity: ";
-  std::cin >> capacity;
+  std::vector<FlightBooking> bookings;
+  //int reserved = 0, capacity = 0;
 
-  std::cout << "Provide number of reserved seats: ";
-  std::cin >> reserved;
+  //FlightBooking booking(1, capacity, reserved);
 
-  FlightBooking booking(1, capacity, reserved);
-
-  booking.printStatus();
-  std::cout << "\nCommands: 'add n', 'cancel n', 'quit'\n";
+  //booking.printStatus();
 
   while (true) {
-    int val1, val2;
+    int id, amount;
     std::string command;
+    if (bookings.size() == 0)
+      std::cout << "No flights in the system\n"
+      << "Commands: 'create/add/cancel n n', 'delete n' 'exit/quit'\n";
+    else {
+      for (FlightBooking booking : bookings) {
+        booking.printStatus();
+        std::cout << std::endl;
+      }
+    }
+    
     std::getline(std::cin, command);
 
-    // handle user input  
+    // handle user input
     if (command.find(' ') != std::string::npos) {
       try {
         // str to int, substr starting of the first found space + 1 to second found space
-        val1 = std::stoi(command.substr(command.find(' ') + 1, command.find(' ', command.find(' '))));
+        id = std::stoi(command.substr(command.find(' ') + 1, command.find(' ', command.find(' '))));
         // str to int, substr starting of the second found space to the rest of the str
-        val2 = std::stoi(command.substr(command.find(' ', command.find(' ') + 1)));
-        std::cout << val1 << val2;
+        amount = std::stoi(command.substr(command.find(' ', command.find(' ') + 1)));
       } catch (...) {
         std::cout << "In 'command n n', n has to be a number!\n";
-        booking.printStatus();
         std::cout << "\nCommands: 'create/add/cancel flight_n seats_n', 'delete flight_n', 'quit' or 'exit'\n";
         continue; 
       }
 
       // create
       if (command.substr(0, command.find(' ')) == "create") {
-        int new_flight_res = booking.getReserved() + val1;
-        if (100 * new_flight_res / booking.getCapacity() > 105)
-          std::cout << "Cannot perform this operation.\n";
-        else {
-          booking.setReserved(new_flight_res);
-          booking.printStatus();
-          std::cout << std::endl;
-        }
+        FlightBooking booking(id, amount);
+        bookings.push_back(booking);
 
       // add
       } else if (command.substr(0, command.find(' ')) == "add") {
-        int new_flight_res = booking.getReserved() + val1;
-        if (100 * new_flight_res / booking.getCapacity() > 105)
-          std::cout << "Cannot perform this operation.\n";
-        else {
-          booking.setReserved(new_flight_res);
-          booking.printStatus();
-          std::cout << std::endl;
-        } 
+        for (FlightBooking& booking : bookings) {
+          if (booking.getId() == id) {
+            int new_flight_res = booking.getReserved() + amount;
+            if (100 * new_flight_res / booking.getCapacity() > 105)
+              std::cout << "Cannot perform this operation.\n";
+            else {
+              booking.setReserved(new_flight_res);
+            }
+          }
+        }
 
       // cancel
       } else if (command.substr(0, command.find(' ')) == "cancel") {
-        int new_flight_res = booking.getReserved() - val1;
-        if (new_flight_res < 0)
-          std::cout << "Cannot perform this operation.\n";
-        else {
-          booking.setReserved(new_flight_res);
-          booking.printStatus();
-          std::cout << std::endl;
+        for (FlightBooking& booking : bookings)
+          if (id == booking.getId()) {
+            int new_flight_res = booking.getReserved() - amount;
+            if (new_flight_res < 0)
+              std::cout << "Cannot perform this operation.\n";
+            else {
+              booking.setReserved(new_flight_res);
+        }
       }
     }
   } // exit
