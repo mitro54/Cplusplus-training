ff4ead69ca33ece830473640a6c759662f295804
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-10-31 16:22:33 +0200

    add setters and getters

--- Essentials 2 (CPA)/flight_booking_system.cpp
+++ Essentials 2 (CPA)/flight_booking_system.cpp
@@ -4,20 +4,35 @@ class FlightBooking {
 public:
   FlightBooking(int id, int capacity, int reserved);
   void printStatus();
+  
+  // getters
+  int getCapacity() { return capacity; };
+  int getReserved() { return reserved; };
+  int getCapacityPercentage();
+
+  // setters
+  void setCapacity(int capacity) { this->capacity = capacity; };
+  void setReserved(int reserved) { this->reserved = reserved; };
 private:
   int id;
   int capacity;
   int reserved;
 };
 
+int FlightBooking::getCapacityPercentage() {
+  return (100 * reserved) / capacity;
+}
+
 void FlightBooking::printStatus() {
   // format: Flight id_n : res_n/cap_n (x%) seats reserved.
   std::cout << "Flight " << id << " : " << reserved << '/' << capacity
-  << " (" << (100 * reserved) / capacity << "%) seats reserved.";
+  << " (" << getCapacityPercentage() << "%) seats reserved.";
 }
 
 FlightBooking::FlightBooking(int id, int capacity, int reserved) {
   this->id = id;
+  int init_percentage = (100 * reserved) / capacity;
+  if (init_percentage > 105) reserved = (capacity * 105) / 100;
   this->capacity = (capacity > 0) ? capacity : 0;
   this->reserved = (reserved > 0) ? reserved : 0;
 }
