4a35acaeca8913d02233f8551450c7a225724509
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-11-01 11:10:49 +0200

    create base code

---
+++ Essentials 2 (CPA)/membership_management_system.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+class Membership {
+  public:
+  // getters
+  // setters
+  private:
+    int id;
+    int valid_for;
+    std::string name;
+};
+
+int main(void) {
+  return 0;
+}
