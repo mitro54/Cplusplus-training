6ea854a00087e5ddc8a8326254e9445dbc672077
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-11-01 12:05:03 +0200

    Add class methods, constructor, printStatus

--- Essentials 2 (CPA)/membership_management_system.cpp
+++ Essentials 2 (CPA)/membership_management_system.cpp
@@ -2,14 +2,35 @@
 #include <string>
 
 class Membership {
-  public:
+public:
+  Membership(int id, int valid_for, std::string name);
+  void printStatus() const;
+
   // getters
+  int getId() const { return id; }
+  int getValidity() const { return valid_for; }
+  std::string getName() const { return name; }
+
   // setters
-  private:
-    int id;
-    int valid_for;
-    std::string name;
+  void setValidity(int id) { this->id = id; }
+  void setName(std::string name) { this->name = name; }
+
+private:
+  int id;
+  int valid_for;
+  std::string name;
 };
+
+Membership::Membership(int id, int valid_for, std::string name) {
+  this->id = id;
+  this->valid_for = valid_for;
+  this->name = name;
+}
+
+void Membership::printStatus() const {
+  std::cout << "Member " << id << " : " << name << ", subscription valid for " 
+  << valid_for << (valid_for == 1) ? " month" : " months"; 
+}
 
 int main(void) {
   return 0;
