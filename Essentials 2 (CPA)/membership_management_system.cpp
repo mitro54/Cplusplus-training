e27970222595bdb0df5c6aa7f335aa311b418ef4
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-11-01 13:36:50 +0200

    fix bugs, add error handling, extend functionality

--- Essentials 2 (CPA)/membership_management_system.cpp
+++ Essentials 2 (CPA)/membership_management_system.cpp
@@ -14,7 +14,7 @@ public:
   std::string getName() const { return name; }
 
   // setters
-  void setValidity(int id) { this->id = id; }
+  void setValidity(int valid_for) { this->valid_for = valid_for; }
   void setName(std::string name) { this->name = name; }
 
 private:
@@ -36,34 +36,45 @@ void Membership::printStatus() const {
 
 int main(void) {
   std::vector<Membership> members;
+
+  // main program loop
   while (true) {
-    std::string cmd, action, name;
     int id, amount;
-    std::cout << "Commands: create/extend id len, delete/cancel id, quit\n";
+    std::string cmd, action, name;
+    std::cout << "Commands: create/extend id len, delete/cancel id, quit\n\n";
+
     if (members.size() == 0)
       std::cout << "No members in the system\n";
     else {
       for (auto& member : members)
         member.printStatus();
-        std::cout << std::endl;
     }
 
     // get the input, use istringstream, then parse input to action
     std::getline(std::cin, cmd);
     std::istringstream iss(cmd);
     iss >> action;
+    iss >> id;
 
-    if (action == "create") {
-      iss >> id >> name;
-      Membership member(id, name);
-      members.push_back(member);
-    } else if (action == "extend") {
-      iss >> id >> amount;
-    } else if (action == "cancel") {
-      iss >> id;
-    } else if (action == "delete") {
-      iss >> id;
-    }
+    try {
+      if (action == "create") {
+        std::getline(iss, name);
+        name = name.substr(1);
+        Membership member(id, name);
+        members.push_back(member);
+      } else if (action == "extend") {
+        iss >> amount;
+        for (auto& member : members) {
+          if (member.getId() == id) {
+            member.setValidity(member.getValidity() + amount);
+          }
+        }
+      } else if (action == "cancel") {
+        iss >> id;
+      } else if (action == "delete") {
+        iss >> id;
+      }
+    } catch (...) { std::cout << "Unable to perform this operation.\n"; }
   }
   return 0;
 }
