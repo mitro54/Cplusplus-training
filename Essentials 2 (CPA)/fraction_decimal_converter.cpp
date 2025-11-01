980d01f3e1b1659fa559bd7496b66c10e2127113
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-11-01 18:10:53 +0200

    add constructor and class methods

--- Essentials 2 (CPA)/fraction_decimal_converter.cpp
+++ Essentials 2 (CPA)/fraction_decimal_converter.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
+#include <cmath>
 
 class Fraction {
 public:
@@ -11,17 +13,33 @@ private:
   int denominator;
 };
 
-// Fraction methods and constructor definition comes here
+Fraction::Fraction(int num, int deno) {
+  this->numerator = num;
+  this->denominator = deno;
+}
+
+std::string Fraction::toString() {
+  if (numerator >= denominator) {}
+  else {
+    return std::to_string(numerator) + '/' + std::to_string(denominator);
+  }
+}
+
+double Fraction::toDouble() {
+  return double(numerator) / denominator;
+}
 
 int main(void) {
   int num, deno;
   std::string input;
   std::cout << "Input a fraction in format numerator / denominator, e.g. 3 / 7\n";
   std::getline(std::cin, input);
-
-  // parse input, set num, deno
+  num = std::stoi(input.substr(0, input.find('/')));
+  deno = std::stoi(input.substr(input.find('/') + 1));
 
   Fraction fraction(num, deno);
-  std::cout << fraction.toString() << " is " << fraction.toDouble() << "in decimal\n";
+  std::cout << std::fixed;
+  std::cout << std::setprecision(2);
+  std::cout << fraction.toString() << " is " << fraction.toDouble() << " in decimal\n";
   return 0;
 }
