b6a60857bbfcf1095dbcad98ae9512a674a05f21
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-11-03 15:01:22 +0200

    quicksave, add bit of logic

--- Essentials 2 (CPA)/2D_distance.cpp
+++ Essentials 2 (CPA)/2D_distance.cpp
@@ -21,8 +21,15 @@ Point2D::Point2D(double x, double y) {
 
 int main(void) {
     std::string input1, input2;
+    double x1, x2, y1, y2;
     std::cout << "Give 2 points in x,y coordinates twice to calculate the distance between the points\n";
     std::getline(std::cin, input1);
     std::getline(std::cin, input2);
+
+    if (input1.find(',') != std::string::npos) {
+        x1 = stod(input1.substr(0, input1.find(',')));
+        x2 = stod(input1.substr(input1.find(',')));
+    }
+
     return 0;
 }
