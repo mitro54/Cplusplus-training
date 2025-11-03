2ecd08aee366440b04e5b858c6639645fa40463c
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-11-03 18:16:11 +0200

    add logic to calculate hypot

--- Essentials 2 (CPA)/2D_distance.cpp
+++ Essentials 2 (CPA)/2D_distance.cpp
@@ -18,6 +18,10 @@ Point2D::Point2D(double x, double y) {
     this->y = y;
 }
 
+double Point2D::distanceTo(Point2D that) {
+  return std::hypot(x - that.x, y - that.y);
+}
+
 
 int main(void) {
     std::string input1, input2;
@@ -27,8 +31,13 @@ int main(void) {
     std::getline(std::cin, input2);
 
     if (input1.find(',') != std::string::npos) {
-        x1 = stod(input1.substr(0, input1.find(',')));
-        x2 = stod(input1.substr(input1.find(',')));
+        x1 = std::stod(input1.substr(0, input1.find(',')));
+        x2 = std::stod(input2.substr(0, input2.find(',')));
+        y1 = std::stod(input1.substr(input1.find(',') + 1));
+        y2 = std::stod(input2.substr(input2.find(',') + 1));
+        Point2D p1(x1, y1);
+        Point2D p2(x2, y2);
+        std::cout << p1.distanceTo(p2);
     }
 
     return 0;
