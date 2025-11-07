ddd3e17c9fb41c89f00c17b9c5ebef482b199722
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-11-07 13:42:17 +0200

    add dequeue

--- Algorithms/array_circ_queue.cpp
+++ Algorithms/array_circ_queue.cpp
@@ -13,12 +13,28 @@ void enqueue(int val) {
     next = (next + 1) % length;
 }
 
+int dequeue() {
+    // make sure theres an item to remove
+    if (next == last) throw std::runtime_error("queue is empty");
+    int length = sizeof(arr) / sizeof(arr[0]);
+    int val = arr[last];
+    last = (last + 1) % length;
+    return val;
+}
+
 int main(void) {
     enqueue(1);
     enqueue(2);
     enqueue(3);
     enqueue(4);
+    
     int length = sizeof(arr) / sizeof(arr[0]);
     for (int i = 0; i < length; i++)
     std::cout << arr[i] << ' ';
+    std::cout << std::endl;
+
+    std::cout << dequeue() << ' ';
+    std::cout << dequeue() << ' ';
+    std::cout << dequeue() << ' ';
+    std::cout << dequeue() << ' ';
 }
