8efecfbdfab98a87b0e6a8e7f4f4fddb9e9928d0
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-11-07 21:43:59 +0200

    add sorting and struct

---
+++ Algorithms/Astar.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+struct Item {
+    int x, y; // grid pos
+    int f; // f = g + h, priority = smaller better
+};
+
+// priority queue as a sorted vector, ascends by f
+std::vector<Item> open_list;
+
+void enqueue(const Item& item) {
+    open_list.push_back(item);
+    int i = open_list.size() - 2;
+    // shift items right while their f is larger than new f
+    while (i >= 0 && open_list[i].f > item.f) {
+        open_list[i + 1] = open_list[i];
+        i--;
+    }
+    // place value at the correct spot
+    open_list[i + 1] = item;
+}
