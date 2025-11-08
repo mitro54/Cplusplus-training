385b8a9e5a20267a5c8eeb4bee92b6324ab971b9
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-11-08 15:05:00 +0200

    add some pseudo

--- Algorithms/Astar.cpp
+++ Algorithms/Astar.cpp
@@ -6,7 +6,8 @@ struct Item {
     int f; // f = g + h, priority = smaller better
 };
 
-// priority queue as a sorted vector, ascends by f
+// priority queue as a sorted vector, will ascend by f
+// we need to use this priority queue implementation for the algorithm
 std::vector<Item> open_list;
 
 void enqueue(const Item& item) {
@@ -27,3 +28,10 @@ Item pop_front() {
     open_list.erase(open_list.begin());
     return best;
 }
+
+// equation F = G + H , G movement cost from point A to given square on grid
+// following the path generated to get there, 10 for x or y, 14 for x & y
+// H is the estimated movemenet cost to move from that given square on the grid to the final destination point B
+// often referred as the heuristic, its a guess
+
+
