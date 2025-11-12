90bbee4c0dbaa193ba7ff73db13f387fd326cf02
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-11-12 17:12:53 +0200

    add destructors

--- Essentials 2 (CPA)/singly_linked_list.cpp
+++ Essentials 2 (CPA)/singly_linked_list.cpp
@@ -1,26 +1,39 @@
 #include <iostream>
 
-class Node
-{
+class Node {
 public:
   Node(int val);
+  ~Node();
   int value;
   Node* next;
 };
 
 Node::Node(int val) : value(val), next(nullptr) {
+  std::cout << "+Node" << std::endl;
+}
+
+Node::~Node() {
+  std::cout << "-Node" << std::endl;
 }
 
 class List {
 public:
   List();
+  ~List();
   void push_front(int value);
   bool pop_front(int &value);
 private:
   Node* head;
 };
 
-List::List() : head(nullptr) {
+List::List() : head(nullptr) {}
+
+List::~List() {
+  while (head != nullptr) {
+    Node* next = head;
+    head = head->next;
+    delete next;
+  }
 }
 
 void List::push_front(int value) {
