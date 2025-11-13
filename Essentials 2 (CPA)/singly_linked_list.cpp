f2bfdbb527616d71ada032abeb887de49f83e574
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-11-13 14:07:15 +0200

    add push_back

--- Essentials 2 (CPA)/singly_linked_list.cpp
+++ Essentials 2 (CPA)/singly_linked_list.cpp
@@ -17,14 +17,16 @@ public:
   List();
   ~List();
   void push_front(int value);
+  void push_back(int value);
   bool pop_front(int &value);
   int size();
 private:
   Node* head;
+  Node* tail;
   int len = 0;
 };
 
-List::List() : head(nullptr) {}
+List::List() : head(nullptr), tail(nullptr) {}
 
 List::~List() {
   while (head != nullptr) {
@@ -36,10 +38,25 @@ List::~List() {
 
 void List::push_front(int value) {
   Node* new_head = new Node(value);
+  if (len == 0) tail = new_head;
   new_head->next = head;
   head=new_head;
   len++;
-  std::cout << "pushed " << value << " size: " << len << "\n";
+  std::cout << "pushed " << value << " to front, size: " << len << "\n";
+}
+
+void List::push_back(int value) {
+  Node* new_tail = new Node(value);
+  new_tail->next = nullptr;
+  if (!head) {
+    head = new_tail;
+    tail = new_tail;
+  } else {
+    tail->next = new_tail;
+    tail = new_tail;
+  }
+  len++;
+  std::cout << "pushed " << value << " to back, size: " << len << "\n";
 }
 
 bool List::pop_front(int &value) {
@@ -66,6 +83,13 @@ int main() {
   std::cout << std::endl;
 
   int value = 0;
+  while (list.pop_front(value)) {}
+  std::cout << std::endl;
+  list.push_back(0);
+  list.push_back(1);
+  list.push_back(2);
+  list.push_back(3);
+  std::cout << std::endl;
   while (list.pop_front(value)) {}
   return 0;
 }
