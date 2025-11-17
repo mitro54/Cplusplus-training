c82d8ee71daef498d4ce882ba43fbed094702878
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-11-17 16:14:01 +0200

    add rest of the logic

--- Essentials 2 (CPA)/polymorphism_part3.cpp
+++ Essentials 2 (CPA)/polymorphism_part3.cpp
@@ -3,32 +3,55 @@
 
 class IPAddress {
 public:
-    IPAddress(int ip) { this->ip = ip; }
+    IPAddress(std::string ip) { this->ip = ip; }
     IPAddress(const IPAddress &source) { ip = source.ip; }
-    virtual void print() { std::cout << ip; }
+    virtual void print() { std::cout << ip << '\n'; }
 protected:
-    int ip;
+    std::string ip;
 };
 
 class IPAddressChecked : public IPAddress {
 public:
-    IPAddressChecked(int ip) : IPAddress(ip) { if (checkIP(ip)) isCorrect = true; };
-    IPAddressChecked(const IPAddressChecked &source) : IPAddress(ip) { ip = source.ip; } // wrong atm
+    IPAddressChecked(std::string ip) : IPAddress(ip) { if (checkIP(ip)) isCorrect = true; };
+    IPAddressChecked(const IPAddressChecked &source) : IPAddress(ip) { ip = source.ip; }
     bool isCorrect = false;
     void print() {
         std::cout << ip << " - ";
-        if (isCorrect) std::cout << "Correct";
-        else std::cout << "Not Correct";
+        if (isCorrect) std::cout << "Correct\n";
+        else std::cout << "Not Correct\n";
     }
+    bool checkIP(std::string ip);
 };
 
-bool checkIP(int ip) {
+bool IPAddressChecked::checkIP(std::string ip) {
+    int count = 0;
+    std::string tester = "0123456789.", num;
 
+    for (int i = 0; i < ip.length(); i++) {
+        if (tester.find(ip[i]) == std::string::npos)
+            return false;
+
+        num.push_back(ip[i]);
+
+        if (num.length() > 4)
+            return false;
+        else if (std::stoi(num) > 255)
+            return false;
+        
+        if (ip[i] == '.') {
+            count++;
+            num = "";
+        }
+    }
+
+    if (count != 3) return false;
+
+    return true;
 }
 
 int main(void) {
-    int ip1, ip2, ip3;
-    std::cout << "enter an ip address to check if its correct: ";
+    std::string ip1, ip2, ip3;
+    std::cout << "enter 3 ip addresses\n";
     std::cin >> ip1;
     std::cin >> ip2;
     std::cin >> ip3;
@@ -36,6 +59,10 @@ int main(void) {
     IPAddress ip_1(ip1);
     IPAddressChecked ip_2(ip2);
     IPAddressChecked ip_3(ip3);
+    std::cout << std::endl << "results:\n";
+    ip_1.print();
+    ip_2.print();
+    ip_3.print();
 
     return 0;
 }
