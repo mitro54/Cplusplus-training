9f0ccdaa88ec8f707912532e57c0d40611c9d50d
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-11-17 15:10:34 +0200

    quicksave

--- Essentials 2 (CPA)/polymorphism_part3.cpp
+++ Essentials 2 (CPA)/polymorphism_part3.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 #include <string>
 
-class IPAddress {};
+class IPAddress {
+public:
+    IPAddress(int ip) { this->ip = ip; }
+    virtual void print() { std::cout << ip; }
+    int ip;
+};
 
-class IPAddressChecked : public IPAddress {};
+class IPAddressChecked : public IPAddress {
+public:
+    IPAddressChecked(IPAddress ip) { checkIP(ip) ? isCorrect = true : isCorrect = false; }
+    bool isCorrect = false;
+};
+
+bool checkIP(IPAddress ip) {
+
+}
 
 int main(void) {
+    int ip1, ip2, ip3;
+    std::cout << "enter an ip address to check if its correct: ";
+    std::cin >> ip1;
+    std::cin >> ip2;
+    std::cin >> ip3;
+
+    IPAddress ip_1(ip1);
+    IPAddressChecked ip_2(ip2);
+    IPAddressChecked ip_3(ip3);
     return 0;
 }
