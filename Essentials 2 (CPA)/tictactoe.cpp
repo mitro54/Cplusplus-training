45bc36e9c54a6b7144563287c9fcffc59e074a1c
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-11-20 11:38:29 +0200

    add getBoard

--- Essentials 2 (CPA)/tictactoe.cpp
+++ Essentials 2 (CPA)/tictactoe.cpp
@@ -3,12 +3,22 @@
 
 class Gameboard {
 public:
-    void getBoard() {}
-    void setMove() {}
+    Gameboard() {};
+    void getBoard();
+    void setMove();
 private:
-    std::string array[9];
+    std::string board[9] = {"_", "_", "_", "_", "_", "_", "_", "_", "_"};
 };
 
+void Gameboard::getBoard() {
+    for (int i = 0; i < 9; i++) {
+        if (i % 3 == 0) std::cout << '\n';
+        std::cout << board[i] << ' ';
+    }
+}
+
 int main(void) {
+    Gameboard board;
+    board.getBoard();
     return 0;
 }
