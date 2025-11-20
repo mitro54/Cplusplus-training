a5a846fadc0492f6f98980b069112d70e5e50207
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-11-20 11:23:36 +0200

    base

---
+++ Essentials 2 (CPA)/tictactoe.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+class Gameboard {
+public:
+    void getBoard() {}
+    void setMove() {}
+private:
+    std::string array[9];
+};
+
+int main(void) {
+    return 0;
+}
