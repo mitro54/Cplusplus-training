9eaf76fe5beb0a70eb495a6aa4b3601d49416814
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-11-20 09:34:38 +0200

    add patternvalidator

--- Essentials 2 (CPA)/password_validator.cpp
+++ Essentials 2 (CPA)/password_validator.cpp
@@ -10,7 +10,7 @@ public:
 class MinLengthValidator : public StringValidator {
 public:
     MinLengthValidator(int input): len(input) {}
-    virtual bool isValid(std::string input) {}
+    virtual bool isValid(std::string input);
 private:
     int len;
 };
@@ -22,13 +22,56 @@ bool MinLengthValidator::isValid(std::string input) {
 class MaxLengthValidator : public StringValidator {
 public:
     MaxLengthValidator(int input): len(input) {}
-    virtual bool isValid(std::string input) {}
+    virtual bool isValid(std::string input);
 private:
     int len;
 };
 
 bool MaxLengthValidator::isValid(std::string input) {
     return (input.size() <= len);
+}
+
+class PatternValidator : public StringValidator {
+public:
+    PatternValidator(std::string input): pattern(input) {}
+    virtual bool isValid(std::string input);
+private:
+    std::string pattern;
+};
+
+bool PatternValidator::isValid(std::string sample) {
+    std::string newstr, nums = "0123456789", chars = "abcdefghijklmnopqrstuvwxyz";
+    std::string punct = " .,=~><|!@#$%^&*()-_+{}[]:\\/\"'";
+
+    for (int i = 0; i < sample.length(); i++) {
+        for (int j = 0; j < pattern.length(); j++) {
+            if (pattern[j] == '?') {
+                newstr.push_back(sample[i]);
+                continue;
+
+            } else if (pattern[j] == 'D') {
+                if (nums.find(sample[i]) != std::string::npos) {
+                    newstr.push_back(sample[i]);
+                    break;
+                } else newstr.erase();
+
+            } else if (pattern[j] == 'A') {
+                if (chars.find(tolower(sample[i])) != std::string::npos) {
+                    newstr.push_back(sample[i]);
+                    break;
+                } else newstr.erase();
+
+            } else if (punct.find(sample[i]) != std::string::npos) {
+                newstr.push_back(sample[i]);
+                break;
+            } else newstr.erase();
+        }
+        if (newstr.length() == pattern.length()) {
+            std::cout << newstr << " " << pattern;
+            return true;
+        }
+    }
+    return false;
 }
 
 void printValid(StringValidator &validator, std::string input) {
