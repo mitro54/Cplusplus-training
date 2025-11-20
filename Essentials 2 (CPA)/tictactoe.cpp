7fbc65747b53c0e3844b3f2d4f1f9db46a4874ab
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-11-20 11:59:19 +0200

    add loop and rest of the logic

--- Essentials 2 (CPA)/tictactoe.cpp
+++ Essentials 2 (CPA)/tictactoe.cpp
@@ -5,20 +5,45 @@ class Gameboard {
 public:
     Gameboard() {};
     void getBoard();
-    void setMove();
+    void setMove(int idx);
+    void setTurn() { turn++; }
 private:
+    int turn = 1;
     std::string board[9] = {"_", "_", "_", "_", "_", "_", "_", "_", "_"};
 };
 
 void Gameboard::getBoard() {
     for (int i = 0; i < 9; i++) {
-        if (i % 3 == 0) std::cout << '\n';
+        if (i % 3 == 0) std::cout << std::endl;
         std::cout << board[i] << ' ';
+    }
+}
+
+void Gameboard::setMove(int idx) {
+    if (idx > 8 || idx < 0) std::cout << "invalid idx";
+    else {
+        turn % 2 == 0 ? board[idx] = 'O' : board[idx] = 'X';
+        setTurn();
     }
 }
 
 int main(void) {
     Gameboard board;
     board.getBoard();
+    std::string input;
+
+    while (true) {
+        std::cout << std::endl << std::endl;
+        std::cin >> input;
+        if (!std::cin) break;
+        if (input == "q") break;
+
+        try {
+            board.setMove(std::stoi(input));
+            board.getBoard();
+        } catch (...) {
+            std::cout << "enter a number 0-8 or q.\n";
+        }
+    }
     return 0;
 }
