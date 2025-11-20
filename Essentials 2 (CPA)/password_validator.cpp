e030d82565d84152036775eeede302579e2e1c02
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-11-20 11:05:29 +0200

    clean up

--- Essentials 2 (CPA)/password_validator.cpp
+++ Essentials 2 (CPA)/password_validator.cpp
@@ -10,26 +10,18 @@ public:
 class MinLengthValidator : public StringValidator {
 public:
     MinLengthValidator(int input): len(input) {}
-    virtual bool isValid(std::string input);
+    virtual bool isValid(std::string input) { return (input.size() >= len); }
 private:
     int len;
 };
-
-bool MinLengthValidator::isValid(std::string input) {
-    return (input.size() >= len);
-}
 
 class MaxLengthValidator : public StringValidator {
 public:
     MaxLengthValidator(int input): len(input) {}
-    virtual bool isValid(std::string input);
+    virtual bool isValid(std::string input) { return (input.size() <= len); }
 private:
     int len;
 };
-
-bool MaxLengthValidator::isValid(std::string input) {
-    return (input.size() <= len);
-}
 
 class PatternValidator : public StringValidator {
 public:
@@ -40,9 +32,8 @@ private:
 };
 
 bool PatternValidator::isValid(std::string pattern) {
-    int d = 0, lc_a = 0, uc_a = 0, p = 0, l = 0;
+    int d = 0, lc_a = 0, uc_a = 0, p = 0;
     for (int i = 0; i < pattern.length(); i++) {
-        if (pattern.length() > 8) l++;
         if (isdigit(pattern[i])) d++;
         if (ispunct(pattern[i])) p++;
         if (isalpha(pattern[i])) {
@@ -51,8 +42,7 @@ bool PatternValidator::isValid(std::string pattern) {
         }
     }
     if (
-        l != 0
-        && d != 0
+        d != 0
         && p != 0
         && lc_a != 0
         && uc_a != 0
@@ -67,10 +57,11 @@ public:
 private:
     MinLengthValidator min_validator;
     MaxLengthValidator max_validator;
-    PatternValidator pattern_validator;
+    PatternValidator   pattern_validator;
 };
 
 PasswordValidator::PasswordValidator(std::string pattern):
+    // adjust min and max to desired range
     min_validator(8), max_validator(32), pattern_validator(pattern) {}
 
 bool PasswordValidator::isValid(std::string input) {
@@ -83,7 +74,7 @@ void printValid(StringValidator &validator, std::string input) {
 }
 
 int main() {
-    std::string input = "Test@test111";
+    std::string input = "Test@tt1";
     PasswordValidator tester(input);
     printValid(tester, input);
     return 0;
