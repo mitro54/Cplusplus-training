967c85f0182abe169621eaf4a6c0b18e2cae77c8
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-11-23 22:27:24 +0200

    quicksave

--- Essentials 2 (CPA)/operator_overloading1.cpp
+++ Essentials 2 (CPA)/operator_overloading1.cpp
@@ -3,7 +3,8 @@
 
 class Matrix {
 public:
-    void add(int n);
+    Matrix(int n1, int n2, int n3, int n4) : matrix{{n1, n2}, {n3, n4}} {}
+    void operator >> (int v);
 private:
     std::vector<std::vector<int>> matrix;
 };
@@ -14,5 +15,7 @@ int main(void) {
     std::cin >> val2;
     std::cin >> val3;
     std::cin >> val4;
+    Matrix m(val1, val2, val3, val4);
+
     return 0;
 }
