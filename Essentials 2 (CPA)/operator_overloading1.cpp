026ea6bafe043af569f09e44dcbd246eafcc45ee
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-11-23 20:49:12 +0200

    add logic

--- Essentials 2 (CPA)/operator_overloading1.cpp
+++ Essentials 2 (CPA)/operator_overloading1.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <vector>
 
-class Container {
+class Matrix {
 public:
+    void add(int n);
 private:
+    std::vector<std::vector<int>> matrix;
 };
 
 int main(void) {
+    int val1, val2, val3, val4;
+    std::cin >> val1;
+    std::cin >> val2;
+    std::cin >> val3;
+    std::cin >> val4;
     return 0;
 }
