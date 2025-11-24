6f347e90dd04f6d73717a322491fd9e95d9af3fc
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-11-24 15:14:14 +0200

    add input

--- Essentials 2 (CPA)/operator_overloading1.cpp
+++ Essentials 2 (CPA)/operator_overloading1.cpp
@@ -3,19 +3,19 @@
 
 class Matrix {
 public:
-    Matrix(int n1, int n2, int n3, int n4) : matrix{{n1, n2}, {n3, n4}} {}
-    void operator >> (int v);
-private:
-    std::vector<std::vector<int>> matrix;
+    int n1, n2, n3, n4;
+    friend std::istream& operator >> (std::istream& in, Matrix& m);
+};
+
+std::istream& operator >> (std::istream& in, Matrix& m) {
+    std::cout << "enter 4 nums: ";
+    in >> m.n1 >> m.n2 >> m.n3 >> m.n4;
+    return in;
 };
 
 int main(void) {
-    int val1, val2, val3, val4;
-    std::cin >> val1;
-    std::cin >> val2;
-    std::cin >> val3;
-    std::cin >> val4;
-    Matrix m(val1, val2, val3, val4);
+    Matrix m;
+    std::cin >> m;
 
     return 0;
 }
