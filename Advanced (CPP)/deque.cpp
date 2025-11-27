23fb2e67ec6693ea4e9565538f36f4a390b1c1c2
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-11-27 15:34:59 +0200

    add base

---
+++ Advanced (CPP)/deque.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <deque>
+
+enum CommandType {
+	MoveLeft,
+	MoveRight,
+	MoveForward,
+	MoveBackward
+};
+
+// add class etc here
+
+int main() {
+	// test code:
+	// RoboCommander commander;
+	// commander.AddCommand(MoveLeft);
+	// commander.AddCommand(MoveRight);
+	// commander.UndoCommand();
+	// commander.UndoCommand();
+	// commander.UndoCommand();
+	// commander.AddCommand(MoveLeft);
+	// commander.AddCommand(MoveForward);
+	// commander.AddCommand(MoveLeft);
+	// commander.AddCommand(MoveForward);
+	// commander.AddCommand(MoveRight);
+	// commander.AddCommand(MoveBackward);
+	// commander.Execute();
+	// commander.UndoCommand();
+	// commander.UndoCommand();
+	// commander.UndoCommand();
+	// commander.UndoCommand();
+	// commander.AddCommand(MoveForward);
+	// commander.Execute();
+	return 0;
+}
