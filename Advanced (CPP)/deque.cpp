604359871585c7cdc1c299342441ef16a658c9b1
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-11-28 17:19:23 +0200

    add rest of the logic

--- Advanced (CPP)/deque.cpp
+++ Advanced (CPP)/deque.cpp
@@ -10,33 +10,46 @@ enum CommandType {
 
 class RoboCommander {
 public:
-	void AddCommand(CommandType cmd);
-	void UndoCommand();
+	void AddCommand(CommandType cmd) { cmd_queue.push_back(cmd); }
+	void UndoCommand() { if (cmd_queue.size() != 0) cmd_queue.pop_back(); }
+	void Execute();
 private:
 	std::deque<CommandType> cmd_queue;
 };
 
+void RoboCommander::Execute() {
+	for (CommandType cmd : cmd_queue) {
+		switch (cmd) {
+			case CommandType::MoveForward: std::cout << "moving forward\n"; break;
+			case CommandType::MoveBackward: std::cout << "moving backwards\n"; break;
+			case CommandType::MoveLeft: std::cout << "moving left\n"; break;
+			case CommandType::MoveRight: std::cout << "moving right\n"; break;
+			default: std::cout << "unknown move\n";
+		}
+	}
+	std::cout << "ready\n";
+}
 
 int main() {
 	// test code:
-	// RoboCommander commander;
-	// commander.AddCommand(MoveLeft);
-	// commander.AddCommand(MoveRight);
-	// commander.UndoCommand();
-	// commander.UndoCommand();
-	// commander.UndoCommand();
-	// commander.AddCommand(MoveLeft);
-	// commander.AddCommand(MoveForward);
-	// commander.AddCommand(MoveLeft);
-	// commander.AddCommand(MoveForward);
-	// commander.AddCommand(MoveRight);
-	// commander.AddCommand(MoveBackward);
-	// commander.Execute();
-	// commander.UndoCommand();
-	// commander.UndoCommand();
-	// commander.UndoCommand();
-	// commander.UndoCommand();
-	// commander.AddCommand(MoveForward);
-	// commander.Execute();
+	RoboCommander commander;
+	commander.AddCommand(MoveLeft);
+	commander.AddCommand(MoveRight);
+	commander.UndoCommand();
+	commander.UndoCommand();
+	commander.UndoCommand();
+	commander.AddCommand(MoveLeft);
+	commander.AddCommand(MoveForward);
+	commander.AddCommand(MoveLeft);
+	commander.AddCommand(MoveForward);
+	commander.AddCommand(MoveRight);
+	commander.AddCommand(MoveBackward);
+	commander.Execute();
+	commander.UndoCommand();
+	commander.UndoCommand();
+	commander.UndoCommand();
+	commander.UndoCommand();
+	commander.AddCommand(MoveForward);
+	commander.Execute();
 	return 0;
 }
