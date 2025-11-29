36ef3656459cc4bdfce950e02316bdecf5961424
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-11-29 12:54:46 +0200

    add first set of iterator loops

--- Advanced (CPP)/iterators.cpp
+++ Advanced (CPP)/iterators.cpp
@@ -2,16 +2,21 @@
 #include <vector>
 
 int main() {
-	std::vector<int> v1;
-	std::vector<int> v2;
+	std::vector<int> v1, v2;
     int max_val = 5;
     std::cout << "enter max value: ";
     // std::cin >> max_val;
+    std::cout << std::endl;
     if (max_val % 2 != 0) max_val++;
     for (int i = 1; i <= max_val; i++)
         (i % 2 == 0) ? v2.push_back(i) : v1.push_back(i);
 
-    // test
-    for (int n : v2) std::cout << '\n' << n;
+    for (std::vector<int>::const_iterator it1 = v1.begin(); it1 != v1.end(); it1++) {
+        for (std::vector<int>::const_iterator it2 = v2.begin(); it2 != v2.end(); it2++) {
+            std::cout << *it1 + *it2 << ' ';
+        }
+        std::cout << std::endl;
+    }
+
 	return 0;
 }
