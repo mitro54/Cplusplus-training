62e64376b4127544ba9b53d6d2282e444bed3649
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-11-29 12:01:54 +0200

    add base

---
+++ Advanced (CPP)/iterators.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+	std::vector<int> v1;
+	std::vector<int> v2;
+    int max_val;
+    std::cout << "enter max value: ";
+    std::cin >> max_val;
+
+	return 0;
+}
