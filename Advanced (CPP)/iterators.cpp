e78bde5ac2858919ba9205dd146a74b6202cfaf1
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-11-29 12:35:13 +0200

    add first loop

--- Advanced (CPP)/iterators.cpp
+++ Advanced (CPP)/iterators.cpp
@@ -4,9 +4,14 @@
 int main() {
 	std::vector<int> v1;
 	std::vector<int> v2;
-    int max_val;
+    int max_val = 5;
     std::cout << "enter max value: ";
-    std::cin >> max_val;
+    // std::cin >> max_val;
+    if (max_val % 2 != 0) max_val++;
+    for (int i = 1; i <= max_val; i++)
+        (i % 2 == 0) ? v2.push_back(i) : v1.push_back(i);
 
+    // test
+    for (int n : v2) std::cout << '\n' << n;
 	return 0;
 }
