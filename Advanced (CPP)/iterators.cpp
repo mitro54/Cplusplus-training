0a4017551aa181c170b8dff4083b92c466a10a0a
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-11-29 12:59:56 +0200

    add second loop

--- Advanced (CPP)/iterators.cpp
+++ Advanced (CPP)/iterators.cpp
@@ -3,9 +3,9 @@
 
 int main() {
 	std::vector<int> v1, v2;
-    int max_val = 5;
+    int max_val;
     std::cout << "enter max value: ";
-    // std::cin >> max_val;
+    std::cin >> max_val;
     std::cout << std::endl;
     if (max_val % 2 != 0) max_val++;
     for (int i = 1; i <= max_val; i++)
@@ -13,6 +13,14 @@ int main() {
 
     for (std::vector<int>::const_iterator it1 = v1.begin(); it1 != v1.end(); it1++) {
         for (std::vector<int>::const_iterator it2 = v2.begin(); it2 != v2.end(); it2++) {
+            std::cout << *it1 + *it2 << ' ';
+        }
+        std::cout << std::endl;
+    }
+
+    // reverses the previous output
+    for (std::vector<int>::reverse_iterator it1 = v1.rbegin(); it1 != v1.rend(); it1++) {
+        for (std::vector<int>::reverse_iterator it2 = v2.rbegin(); it2 != v2.rend(); it2++) {
             std::cout << *it1 + *it2 << ' ';
         }
         std::cout << std::endl;
