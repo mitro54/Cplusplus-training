13c520db3ff92c1e965a620ef2f6ecbe0bd9d6a5
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-12-01 20:17:49 +0200

    add base

---
+++ Advanced (CPP)/set_count.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <set>
+#include <functional>
+ 
+// add code
+  
+int main() {
+std::set <double, std::greater<double>> valuesA = { -1.1, 2.9, -2.3, 2.71 };
+std::set <double, std::greater<double>> valuesB = { -3.14, 2.71, -3.88, 2.19 };
+
+double value;
+std::cin >> value;
+
+//addcode
+
+return 0;
+}
