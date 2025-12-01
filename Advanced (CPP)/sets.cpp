06d6f14087396f7d8075fafe86075b46fa7016db
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-12-01 18:39:31 +0200

    add base

---
+++ Advanced (CPP)/sets.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <set>
+
+int main() {
+	std::set<int> s1, s2;
+	unsigned start_val;
+	std::cin >> start_val;
+	unsigned stop_val;
+	std::cin >> stop_val;
+	
+	// logic here
+	return 0;
+}
