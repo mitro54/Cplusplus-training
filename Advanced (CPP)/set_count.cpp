f4758fbbfcf6b2023d44d2d4908171c0eecb66ae
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-12-02 17:23:41 +0200

    add logic

--- Advanced (CPP)/set_count.cpp
+++ Advanced (CPP)/set_count.cpp
@@ -2,7 +2,11 @@
 #include <set>
 #include <functional>
  
-// add code
+template <class T> void print(T start, T end) {
+    for (; start != end; ++start) {
+        std::cout << *start << " ";
+    }
+}
   
 int main() {
 std::set <double, std::greater<double>> valuesA = { -1.1, 2.9, -2.3, 2.71 };
@@ -11,7 +15,17 @@ std::set <double, std::greater<double>> valuesB = { -3.14, 2.71, -3.88, 2.19 };
 double value;
 std::cin >> value;
 
-//addcode
+if (valuesA.count(value) == 0) std::cout << "Not found";
+else {
+    print(valuesA.find(value), valuesA.end());
+}
+
+std::cout << std::endl;
+
+if (valuesB.count(value) == 0) std::cout << "Not found";
+else {
+    print(valuesB.find(value), valuesB.end());
+}
 
 return 0;
 }
