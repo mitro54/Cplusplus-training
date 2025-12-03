17d6116785633ea70363d5c3973ec5b642a21628
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-12-03 15:10:42 +0200

    add base

---
+++ Advanced (CPP)/maps.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+
+
+int main() {
+    std::map<std::string, int> variables_map = { {"x", 0}, {"y", 1}, {"z", 2}, { "xx", 3 },{ "xy", 4 },{ "xz", 5 } };
+	std::vector<int> values = { 9, 6, 5, 7, 3, 2};
+	std::string variable_name;
+	std::cin >> variable_name;
+
+	// code
+	
+	return 0;
+}
