739e915e6dd9135b9e780433e5efd6e4e3a5e91f
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-12-04 14:07:44 +0200

    add the logic

--- Advanced (CPP)/for_each.cpp
+++ Advanced (CPP)/for_each.cpp
@@ -2,13 +2,14 @@
 #include <algorithm>
 #include <vector>
 
-//
+void print_square(int &value) {
+    std::cout << value * value << ' ';
+}
 
-int main()
-{
-	std::vector<int>v1 = { 3, 5, 6, 8, 9};
-
-	//
-
+int main() {
+	std::vector<int>v1 = { 3, 5, 6, 8, 9 };
+	for_each(v1.begin(), v1.end(), print_square);
+    std::cout << std::endl;
+	for_each(v1.rbegin(), v1.rend(), print_square);
 	return 0;
 }
