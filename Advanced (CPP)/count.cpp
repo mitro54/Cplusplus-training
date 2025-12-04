156d301998670e4809dd3657f1c4cdd798949492
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-12-04 19:22:38 +0200

    base

---
+++ Advanced (CPP)/count.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+//code
+
+int main() {
+	std::vector<double> values = 
+		{ 2.1, 3.14, 2.2, 1.1, 2.11, 3.2, 3.6, 2.5, 2.6, 4.11, 4.12, 3.11, 3.73, 4.83};
+	unsigned start_value;
+	std::cin >> start_value;
+	unsigned stop_value;
+	std::cin >> stop_value;
+	//code
+	return 0;
+}
