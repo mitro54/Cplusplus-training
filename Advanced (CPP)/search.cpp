e4a1e6e4d8488a6aae53a0f38780e481d207ebdd
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-12-04 18:29:20 +0200

    add base

---
+++ Advanced (CPP)/search.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+//
+
+int main() {
+	std::vector<int> values = { 1, 1, 5, 3, 4, 4, 3, 2, 2, 4, 4, 5, 3, 8, 8, 1 };
+	// pattern 1: { 0, 1, -1 }
+	// pattern 2: { -1, 1, 0 }
+	return 0;
+}
