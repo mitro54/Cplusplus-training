58bddc57c7203b99e6955808ce72446cd27576de
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-12-04 14:21:39 +0200

    add base

---
+++ Advanced (CPP)/find_ops.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+
+int main() {
+	std::map<std::string, std::string> capitals = { 
+		{"China", "Beijing"}, 
+		{"India", "New Delhi"}, 
+		{"Japan", "Tokyo" },
+		{"Philippines", "Manila" },
+		{"Poland", "Warsaw" },
+		{"Egypt", "Cairo" },
+		{"Indonesia", "Jakarta" },
+		{"Democratic Republic of the Congo", "Kinshasa" },
+		{"South Korea", "Seoul" },
+		{"Bangladesh", "Dhaka" },
+		{"Iceland", "Reykjavík" }
+	};
+	std::string s;
+	getline(std::cin, s);
+
+	// code
+	
+	return 0;
+}
