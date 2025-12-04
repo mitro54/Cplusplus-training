f49776c842c2e3f68b35011710becafb3b8bc172
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-12-04 19:35:31 +0200

    add base

---
+++ Advanced (CPP)/mismatch.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+// code
+
+int main() {
+	std::vector<int> original_values = { 210, 314, 202, 110, 211, 302, 358, 229, 260, 411, 412, 311, 373, 483 };
+	std::vector<int> noisy_values =    { 211, 324, 203, 113, 227, 320, 340, 210, 239, 411, 412, 333, 371, 501 };
+
+	// code
+
+	return 0;
+}
