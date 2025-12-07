6ad7bcbca0f330b7105d4505acd9aad9e61e1dd5
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-12-07 20:56:00 +0200

    add base code

---
+++ Advanced (CPP)/transform.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+// code
+
+int main() {
+	std::vector<int> valuesA = { 1, 5, 2, 6, 3 };
+	std::vector<int> valuesB = { 2, 3, 1, 3, 4 };
+	std::vector<int> valuesC = { 2, 1, 5, 2, 1 };
+	std::vector<int> results(valuesA.size());
+	int value;
+	std::cin >> value;
+
+	//code
+
+	return 0;
+}
