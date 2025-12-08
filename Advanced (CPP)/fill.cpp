4e475c1714f5e25fbd221bb3c1e7aff5ce69dc43
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-12-08 11:14:19 +0200

    add base

---
+++ Advanced (CPP)/fill.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+// code
+
+int main() {
+	std::vector<int> values = { 6, 5, 7, 3, 4, 6, 5, 7, 3, 4 };
+
+	// code
+
+	std::cout << '\n';
+
+	return 0;
+}
