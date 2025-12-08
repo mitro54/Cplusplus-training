7616f42d24ff92cc0a51fb6c1dd659e97236a7c0
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-12-08 11:37:59 +0200

    base

---
+++ Advanced (CPP)/generate.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+
+// code
+
+int main() {
+	std::vector<int> resultsSmall(6);
+	std::vector<int> results;
+	int count;
+	std::cin >> count;
+
+	// code
+	
+	return 0;
+}
