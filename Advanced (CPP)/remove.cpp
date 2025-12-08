035137b13e313952e4f4f4493c8cfc2b0cb5fe38
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-12-08 12:39:44 +0200

    add base

---
+++ Advanced (CPP)/remove.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+// code
+
+int main() {
+	std::vector<int> results;
+	int count;
+	std::cin >> count;
+	
+	// code
+	
+	return 0;
+}
