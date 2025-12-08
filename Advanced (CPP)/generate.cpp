47aefe7d5cbda5440a3a69a4fbc83608bd02310b
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-12-08 12:33:59 +0200

    add logic

--- Advanced (CPP)/generate.cpp
+++ Advanced (CPP)/generate.cpp
@@ -3,16 +3,29 @@
 #include <vector>
 #include <algorithm>
 
-
-// code
+//  Tn = n(n + 1) / 2
+struct Triangulate {
+    int n;
+    Triangulate(int start_n = 1) : n(start_n) {};
+    int operator() () {
+    int result = (n * (n + 1)) / 2;
+    n++;
+    return result;
+  }
+};
 
 int main() {
 	std::vector<int> resultsSmall(6);
-	std::vector<int> results;
 	int count;
 	std::cin >> count;
+	std::vector<int> results(count);
 
-	// code
+	std::generate(resultsSmall.begin(), resultsSmall.end(), Triangulate());
+	std::generate_n(results.begin(), count, Triangulate());
+
+    for (int i : resultsSmall) std::cout << i << ' ';
+    std::cout << '\n';
+    for (int i : results) std::cout << i << ' ';
 	
 	return 0;
 }
