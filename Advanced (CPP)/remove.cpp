8ef7f3437703607872fd6b090ca15ecd6ae77dbd
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-12-09 20:06:48 +0200

    add the logic

--- Advanced (CPP)/remove.cpp
+++ Advanced (CPP)/remove.cpp
@@ -3,14 +3,44 @@
 #include <vector>
 #include <algorithm>
 
-// code
+struct Counter {
+    int n;
+    Counter() : n(1) {};
+    int operator() () {
+    return n++;
+  }
+};
+
+bool isThreeIncluded(int n) {
+	n = std::abs(n);
+	while (n > 0) {
+		int digit = n % 10;
+		if (digit == 3) return true;
+		n /= 10;
+	}
+	return false;
+}
+
+struct FilterThree {
+  bool operator () (const int &a) {
+    if (a % 3 == 0) return true;
+	if (isThreeIncluded(a)) return true;
+	return false;
+  }
+};
 
 int main() {
 	std::vector<int> results;
 	int count;
 	std::cin >> count;
+	results.resize(count);
 	
-	// code
+	std::generate(results.begin(), results.end(), Counter());
+	auto new_end = std::remove_if(results.begin(), results.end(), FilterThree());
+	results.erase(new_end, results.end());
+
+	for (int i : results) std::cout << i << ' ';
+
 	
 	return 0;
 }
