780406d30a4ea941daddd088d6111ca1a569138f
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-12-10 14:10:06 +0200

    add logic

---
+++ Advanced (CPP)/rotate.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+int main() {
+    // rotate the rotated vector to match the values vector.
+	std::vector<int> values = { 8, 1, 5, 6, 7 };
+	std::vector<int> rotated = {5, 6, 7, 8, 1};
+
+    std::vector < int >::iterator it = rotated.begin();
+    advance(it, 3);
+    rotate(rotated.begin(), it, rotated.end());
+  
+	if (rotated== values) std::cout << "You did it" << '\n';
+	else std::cout << "Try again" << '\n';
+	return 0;
+}
