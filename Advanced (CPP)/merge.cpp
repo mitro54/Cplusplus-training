841459e963aab1d06ec7f95721517a710dd584bb
mitro vaskela <152814815+mitro54@users.noreply.github.com>
2025-12-11 18:20:55 +0000

    add base

---
+++ Advanced (CPP)/merge.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include <vector>
+
+#include <algorithm>
+
+#include <iterator>
+
+// code
+
+int main() {
+    vector <Point> pointsA = { { 1, 2 },{ 3, 5 },{ 2, 2 } };
+    vector <Point> pointsB = { { 3, 2 },{ 2, 1 },{ 4, 1 } };
+    vector <Point> pointsClosest;
+     // code
+    return 0;
+}
