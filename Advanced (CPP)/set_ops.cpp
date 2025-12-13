2f3f8bd264aeb3c323ab5b81aaf4b1102fa4026a
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-12-13 23:34:59 +0200

    add base

---
+++ Advanced (CPP)/set_ops.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+struct Country {
+	std::string name;
+	int area;
+};
+
+// code
+
+int main() {
+	std::vector <Country> countries = {
+		{ "India", 3287 },{ "Argentina", 2780 },
+		{ "Brazil", 8515 },{ "Australia", 7692 },
+		{ "China", 9572 },{ "United States of America", 9525 },
+		{ "Russia", 17098 },{ "Canada", 9984 },
+		{ "Kazakhstan", 	2724 },{ "Algeria", 2381 }
+	};
+	std::vector <Country> countries_small = {
+		{ "Brazil", 8515 },{ "Australia", 7692 },
+		{ "Kazakhstan", 	2724 },{ "Algeria", 2381 }
+	};
+
+	// code
+	
+	return 0;
+}
