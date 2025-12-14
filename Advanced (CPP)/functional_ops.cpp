4d9e952d13bbee8041647ddbb74b27f94a1f4c16
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-12-14 15:20:18 +0200

    base

---
+++ Advanced (CPP)/functional_ops.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <functional>
+#include <vector>
+#include <numeric>
+
+
+// code
+
+int main()
+{
+	std::vector<double> student1Grades = { 3, 4, 5, 4, 4.5, 5, 3.5, 4 };
+	std::vector<double> student2Grades = { 5, 4.5, 5, 4.5, 3, 3, 3.5, 5 };
+	std::vector<double> gradesWeights =  { 0.05, 0.15, 0.1, 0.2, 0.05, 0.3, 0.1, 0.05 };
+	std::vector<double> weighted1(8);
+	std::vector<double> weighted2(8);
+
+	// code
+	
+	return 0;
+}
