ca431ef86566f620166e4fb9615292b8103effe0
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-12-14 15:24:54 +0200

    add code

--- Advanced (CPP)/comparison_ops.cpp
+++ Advanced (CPP)/comparison_ops.cpp
@@ -4,7 +4,11 @@
 #include <functional>
 #include <vector>
 
-// code
+template <class T> void print(T start, T end) {
+	for (; start != end; ++start) {
+		std::cout << *start << " ";
+	}
+}
 
 int main() {
 	std::vector<int> v1 = { 3, 4, 5, 4, 4, 5, 3, 4 };
@@ -12,8 +16,13 @@ int main() {
 	std::vector<int> resultsEqual(v1.size());
 	std::vector<int> resultsLess(v1.size());
 	std::vector<int> resultsGreater(v1.size());
-	
-	// code
-	
+	transform(v1.begin(), v1.end(), v2.begin(), resultsEqual.begin(), std::equal_to<int>());
+	transform(v1.begin(), v1.end(), v2.begin(), resultsLess.begin(), std::less <int>());
+	transform(v1.begin(), v1.end(), v2.begin(), resultsGreater.begin(), std::greater<int>());
+	print(v1.begin(), v1.end()); std::cout << std::endl;
+	print(v2.begin(), v2.end()); std::cout << std::endl;
+	print(resultsEqual.begin(), resultsEqual.end()); std::cout << std::endl;
+	print(resultsLess.begin(), resultsLess.end()); std::cout << std::endl;
+	print(resultsGreater.begin(), resultsGreater.end()); std::cout << std::endl;
 	return 0;
 }
