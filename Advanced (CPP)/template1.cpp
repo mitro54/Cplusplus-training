d5008416b5a0f417565b5bff540326c3e68b88ea
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-12-14 19:10:36 +0200

    add code

---
+++ Advanced (CPP)/template1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <sstream>
+
+template<class T> int signum(T value) {
+	if (value < 0)
+		return -1;
+	if (value > 0)
+		return 1;
+	return 0;
+}
+
+template<class T> std::string toString(T value) {
+	stringstream s;
+	s << value;
+	return s.str();
+}
+
+
+int main() {	
+	int valuei ;
+	double valued ;
+	std::cin >> valuei;
+	std::cin >> valued;
+	std::cout << signum(valuei) << std::endl;
+	std::cout << signum(valued) << std::endl;
+	std::cout << toString(valuei) << std::endl;
+	std::cout << toString(valued) << std::endl;
+	return 0;
+}
