2cd62ebb9b03bb957bd73966748e9ab8a7cc125d
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-12-14 17:34:47 +0200

    add code

--- Advanced (CPP)/io.cpp
+++ Advanced (CPP)/io.cpp
@@ -4,9 +4,39 @@
 #include <iomanip>
 
 int main() {
-	std::string filename  = "filename.txt";
-	
-	// code
-	
-	return 0;
+    std::string filename = "Readme.txt";
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cout << "File error: couldn't open." << std::endl;
+        return 1;
+    }
+
+    std::ofstream file_copy("copy.txt");
+    if (!file_copy.is_open()) {
+        std::cout << "File error: couldn't open." << std::endl;
+        return 1;
+    }
+
+    std::string line;
+    int lineNumber = 1;
+    while (getline(file, line)) {
+        file_copy << lineNumber << ":" << line << std::endl;
+        lineNumber++;
+    }
+
+    file.close();
+    file_copy.close();
+
+    std::ifstream file_display("copy.txt");
+    if (!file_display.is_open()) {
+        std::cout << "File error: couldn't open." << std::endl;
+        return 1;
+    }
+
+    for (int j = 0; j < 3 && getline(file_display, line); ++j) {
+        std::cout << line << std::endl;
+    }
+
+    file_display.close();
+    return 0;
 }
