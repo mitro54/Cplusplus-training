c74ab8d53cff019b9cb013ac29e5a7ffb1c15bdf
mitro54 <152814815+mitro54@users.noreply.github.com>
2025-12-14 15:23:51 +0200

    base

---
+++ Advanced (CPP)/comparison_ops.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <functional>
+#include <vector>
+
+// code
+
+int main() {
+	std::vector<int> v1 = { 3, 4, 5, 4, 4, 5, 3, 4 };
+	std::vector<int> v2 = { 3, 3, 2, 1, 5, 6, 9, 4 };
+	std::vector<int> resultsEqual(v1.size());
+	std::vector<int> resultsLess(v1.size());
+	std::vector<int> resultsGreater(v1.size());
+	
+	// code
+	
+	return 0;
+}
